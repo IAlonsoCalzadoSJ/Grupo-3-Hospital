317f2364b1b5c616ce0d13f47262e964258e88d8
Israel Alonso Calzado <ialonsocalzado.sanjose@alumnado.fundacionloyola.net>
2022-12-12 13:00:36 +0100

    v0

---
+++ src/main/java/principal/Inicio.java
@@ -0,0 +1,20 @@
+package principal;
+/**
+ * 
+ */
+
+/**
+ * @author Israel
+ *
+ */
+public class Inicio {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
