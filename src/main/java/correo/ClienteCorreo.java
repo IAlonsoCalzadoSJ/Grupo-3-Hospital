63eb745337bd1985c1ae1bddf4f25e2f4b24b4a8
Álvaro Palacios Cabrera <alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net>
2022-12-14 10:33:52 +0100

    Correo enviar

--- src/main/java/correo/ClienteCorreo.java
+++ src/main/java/correo/ClienteCorreo.java
@@ -3,10 +3,23 @@
  */
 package correo;
 
+import java.util.Properties;
+
 /**
  * @author Israel
  *
  */
-public class ClienteCorreo {
+public class ClienteCorreo {	
 
+	public static void main(String[] args) {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.user", "alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net");
+		properties.put("mail.password", "password");
+		
+		Session session = Session.getInstance(properties, null);;
+	}
 }
