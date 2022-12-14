cab80573ac5b88a767a8cf9a7b48407492e378eb
Álvaro Palacios Cabrera <alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net>
2022-12-14 13:51:51 +0100

    Correo mandar mensaje

--- src/main/java/correo/ClienteCorreo.java
+++ src/main/java/correo/ClienteCorreo.java
@@ -3,23 +3,55 @@
  */
 package correo;
 
+import java.io.IOException;
 import java.util.Properties;
 
-/**
- * @author Israel
- *
- */
-public class ClienteCorreo {	
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class ClienteCorreo {
 
 	public static void main(String[] args) {
-		Properties properties = new Properties();
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.port", "587");
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.user", "alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net");
-		properties.put("mail.password", "password");
-		
-		Session session = Session.getInstance(properties, null);;
+	
+	String remitente = "alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net";
+	String claveemail = "";
+	
+	Properties properties = System.getProperties();	
+	properties.put("mail.smtp.host","smtp.gmail.com");
+	properties.put("mail.smtp.user",remitente);
+	properties.put("mail.smtp.clave",claveemail);
+	properties.put("mail.smtp.auth","true");
+	properties.put("mail.smtp.starttls.enable", "true");
+	properties.put("mail.smtp.port", "587");
+	Session session = Session.getDefaultInstance(properties);
+    MimeMessage message = new MimeMessage(session);
+
+    try {
+        message.setFrom(new InternetAddress(remitente));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress("miguelangelmeridamedina.sanjose@alumnado.fundacionloyola.net"));   //Se podrían añadir varios de la misma manera
+        message.setSubject("Probando mandar mensajes con imagenes");
+        message.setText("Esto es una prueba de mensaje adjuntando");
+        MimeMultipart mimeMultipart = new MimeMultipart();
+        MimeBodyPart mimeBodyPartTexto = new MimeBodyPart();
+        mimeBodyPartTexto.setText("Probando mandar fotos");
+        MimeBodyPart mimeBodyPartAdjunto = new MimeBodyPart();
+        mimeBodyPartAdjunto.attachFile("C:\\Users\\alvar\\Desktop\\imagen\\muneconieve.jpg");
+        mimeMultipart.addBodyPart(mimeBodyPartAdjunto);
+        mimeMultipart.addBodyPart(mimeBodyPartTexto);
+        message.setContent(mimeMultipart);
+        Transport transport = session.getTransport("smtp");
+        transport.connect("smtp.gmail.com", remitente, claveemail);
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+    }
+    catch (MessagingException | IOException me) {
+        me.printStackTrace();   //Si se produce un error
+    }
 	}
 }
