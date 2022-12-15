6f94c8dbda92bbcf6a893f7f991048a3ce0ae768
Jose Francisco Rodríguez Portela <95849564+JoseFran57115@users.noreply.github.com>
2022-12-15 14:13:14 +0100

    Prueba con vistaFTP
    Vista Swing añadida

---
+++ src/main/java/ftp/VentanaSwingFTP.java
@@ -0,0 +1,126 @@
+package ftp;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JList;
+
+public class VentanaSwingFTP extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaSwingFTP frame = new VentanaSwingFTP();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaSwingFTP() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 811, 542);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnSubir = new JButton("Subir Archivo");
+		btnSubir.setHorizontalAlignment(SwingConstants.LEFT);
+		btnSubir.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnSubir.setBounds(46, 35, 200, 54);
+		contentPane.add(btnSubir);
+		
+		JButton btnBajar = new JButton("Bajar Archivo");
+		btnBajar.setHorizontalAlignment(SwingConstants.LEFT);
+		btnBajar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnBajar.setBounds(46, 98, 200, 54);
+		contentPane.add(btnBajar);
+		
+		JButton btnBorrarAr = new JButton("Borrar Archivo");
+		btnBorrarAr.setHorizontalAlignment(SwingConstants.LEFT);
+		btnBorrarAr.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnBorrarAr.setBounds(46, 162, 200, 54);
+		contentPane.add(btnBorrarAr);
+		
+		JButton btnRenombrar = new JButton("Renombrar Archivo");
+		btnRenombrar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnRenombrar.setHorizontalAlignment(SwingConstants.LEFT);
+		btnRenombrar.setBounds(46, 226, 200, 54);
+		contentPane.add(btnRenombrar);
+		
+		JButton btnCrearCarpeta = new JButton("Icono Carpeta");
+		btnCrearCarpeta.setBounds(46, 300, 91, 54);
+		contentPane.add(btnCrearCarpeta);
+		
+		JButton btnBorrarCarpeta = new JButton("Icono Borrar\r\n");
+		btnBorrarCarpeta.setBounds(155, 300, 91, 54);
+		contentPane.add(btnBorrarCarpeta);
+		
+		JLabel lblServidor = new JLabel("Servidor:");
+		lblServidor.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		lblServidor.setHorizontalAlignment(SwingConstants.CENTER);
+		lblServidor.setBounds(51, 386, 86, 25);
+		contentPane.add(lblServidor);
+		
+		JLabel lblIpServer = new JLabel("New label");
+		lblIpServer.setHorizontalAlignment(SwingConstants.CENTER);
+		lblIpServer.setBounds(155, 386, 120, 25);
+		contentPane.add(lblIpServer);
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsuario.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		lblUsuario.setBounds(51, 428, 86, 25);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(155, 428, 120, 25);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblDirectorio = new JLabel("Directorio:");
+		lblDirectorio.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		lblDirectorio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDirectorio.setBounds(323, 10, 158, 34);
+		contentPane.add(lblDirectorio);
+		
+		JLabel lblRutaDirectorio = new JLabel("Ruta");
+		lblRutaDirectorio.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblRutaDirectorio.setBounds(529, 10, 221, 34);
+		contentPane.add(lblRutaDirectorio);
+		
+		JList listArchivos = new JList();
+		listArchivos.setBounds(323, 98, 427, 355);
+		contentPane.add(listArchivos);
+		
+		JLabel lblDirActual = new JLabel("Directorio Actual:");
+		lblDirActual.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		lblDirActual.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDirActual.setBounds(323, 54, 158, 34);
+		contentPane.add(lblDirActual);
+		
+		JLabel lblRutaDirActual = new JLabel("Ruta");
+		lblRutaDirActual.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblRutaDirActual.setBounds(529, 56, 221, 33);
+		contentPane.add(lblRutaDirActual);
+	}
+}
---
+++ src/main/java/ftp/main.java
@@ -0,0 +1,10 @@
+package ftp;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new VistaFTP();
+	}
+
+}
