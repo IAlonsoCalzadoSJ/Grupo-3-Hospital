1c1692b005c634f19f808478b1108502600252f0
Álvaro Palacios Cabrera <alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net>
2022-12-19 11:36:47 +0100

    Email

--- src/main/java/correo/ClienteCorreo.java
+++ src/main/java/correo/ClienteCorreo.java
@@ -22,10 +22,8 @@ public class ClienteCorreo {
 	
 	public static void main(String[] args) {
 	
-<<<<<<< Updated upstream
 	String remitente = "alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net";
 	String claveemail = "rlfcwbbjbeaygsoe";
-=======
 		Properties properties = System.getProperties();	
 		properties.put("mail.smtp.host","smtp.gmail.com");
 		properties.put("mail.smtp.user",remitente);
@@ -35,18 +33,14 @@ public class ClienteCorreo {
 		properties.put("mail.smtp.port", "587");
 		Session session = Session.getDefaultInstance(properties);
 	    MimeMessage message = new MimeMessage(session);
->>>>>>> Stashed changes
 	
 	    
     try {
         message.setFrom(new InternetAddress(remitente));
-<<<<<<< Updated upstream
         message.addRecipient(Message.RecipientType.TO, new InternetAddress("miguelangelmeridamedina.sanjose@alumnado.fundacionloyola.net"));   //Se podrían añadir varios de la misma manera
         message.setSubject("Probando jtable3");
-=======
         message.addRecipient(Message.RecipientType.TO, new InternetAddress("alvaropalacioscabrera.sanjose@alumnado.fundacionloyola.net"));   //Se podrían añadir varios de la misma manera
         message.setSubject("Probando mandar mensajes con imagenes parte 28");
->>>>>>> Stashed changes
         message.setText("Esto es una prueba de mensaje adjuntando");
         MimeMultipart mimeMultipart = new MimeMultipart();
         MimeBodyPart mimeBodyPartTexto = new MimeBodyPart();
